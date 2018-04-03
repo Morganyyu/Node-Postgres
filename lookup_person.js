67574e8ae0e1dbda7360ea1dee372e82aefdb9c7
Morganyyu <morganyu120@hotmail.com>
2018-04-03 20:35:32 +0000

    now you can look up from database based on first or last name

---
+++ lookup_person.js
@@ -0,0 +1,22 @@
+const pg = require('pg');
+const settings = require("./settings");
+const name = [process.argv[2]];
+const client = new pg.Client(settings);
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  client.query("SELECT * FROM famous_people WHERE first_name = $1 OR last_name = $1", name, (err, result) => {
+    console.log("Searching...");
+    if (err) {
+      return console.error("error running query", err);
+    }
+    result.rows.forEach(function(person) {
+      let birthday = person.birthdate.toISOString().slice(0,10);
+      console.log(`Found ${result.rowCount} person(s) by the name of '${name}':`)
+      console.log(`- ${person.id}: ${person.first_name} ${person.last_name}, born '${birthday}'`);
+    })
+    client.end();
+  })
+});
